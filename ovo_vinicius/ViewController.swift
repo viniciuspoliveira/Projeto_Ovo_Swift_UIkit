940d233604f99848bebb8984fa609efebcb97365
Vinicius Pinheiro <viniciusp.penha@gmail.com>
2022-10-02 09:55:16 -0300

    Primeiro commit

--- ovo_vinicius/ViewController.swift
+++ ovo_vinicius/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Gema Mole": 3, "Gema Cremosa": 4, "Gema Cozida": 7]
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        
+        timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = "Como você gostaria da fritura dos ovos?"
+        
+        
+    }
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+  
+    
+        timer.invalidate()
+        
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let hardness = sender.currentTitle! //Soft, Medium, Hard
+    
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
     }
 
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float (secondsPassed) / Float (totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
 
+
+
+
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Seu ovo ta pronto amigo! Corra pra panela!"
+
+        }
+    }
 }
-
